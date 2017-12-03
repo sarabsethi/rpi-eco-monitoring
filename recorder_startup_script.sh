85348dcd9c07516c9be3fc9692c041935bff01df
sarabsethi <sarab.sethi93@gmail.com>
2017-12-03 23:40:17 +0000

    better internet checking. use printf not echo

--- recorder_startup_script.sh
+++ recorder_startup_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'Start of ecosystem monitoring startup script'
+printf '############################################\nStart of ecosystem monitoring startup script\n############################################\n'
 
 # just as a back up schedule a reboot for 24 hours (in case something goes wrong before scheduling the 2am reboot)
 (sudo shutdown -r +1440) &
@@ -13,19 +13,17 @@ cd /home/pi/rpi-eco-monitoring
 # Restart udev to simulate hotplugging of 3G dongle
 bash ./bash_restart_udev.sh
 
-# Sleep until internet connection is up, or we've been trying for 1 minute
 tries=0
 while true; do
-	ping -c 1 -W 1 www.google.com
-	ping_resp=$?
-	if [[ $ping_resp -eq 0 ]] ;then
-		dongle_page=$(ping -c 1 -W 1 www.google.com | grep -qi 'hi.link')
-		if [[ $dongle_page -eq 1 ]] ;then
-			break
-		fi
+	timeout 2s wget -q --spider http://google.com
+	if [ $? -eq 0 ]; then
+		printf "Online"
+    break
+	else
+	    printf "Offline"
 	fi
 
-	echo 'Waiting for internet connection before continuing (10 tries max)'
+	printf 'Waiting for internet connection before continuing (10 tries max)'
 	sleep 1
 
 	let tries=tries+1
@@ -45,12 +43,12 @@ last_sha=$(git rev-parse HEAD)
 #git pull
 git fetch origin
 git reset --hard origin/master
-echo 'Pulled from github'
+printf 'Pulled from github'
 now_sha=$(git rev-parse HEAD)
 
 # Check if this file has changed - reboot if so
 changed_files="$(git diff-tree -r --name-only --no-commit-id $last_sha $now_sha)"
-echo "$changed_files" | grep --quiet "recorder_startup_script" && sudo reboot
+printf "$changed_files" | grep --quiet "recorder_startup_script" && sudo reboot
 
 # Add in current date and time to log files
 currentDate=$(date +"%Y-%m-%d_%H.%M")
@@ -63,5 +61,5 @@ piId=$(cat /proc/cpuinfo | grep Serial | cut -d ' '  -f 2)
 logFileName="$piId""_""$currentDate"_log.txt
 
 # Start recording script
-echo 'End of startup script'
+printf 'End of startup script'
 sudo python -u python_record.py |& tee $logFileName
