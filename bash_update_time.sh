fe2fa94c623a6e2164c802af318145c2de64cdb2
sarabsethi <sarab.sethi93@gmail.com>
2017-12-05 12:57:47 +0000

    change time updating logic

--- bash_update_time.sh
+++ bash_update_time.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Update time from internet
-sudo /etc/init.d/ntp stop
-sudo timeout 300s sntp -s 24.56.178.140
+sudo timeout 300s ntpdate ntp.ubuntu.com
 
 if (($? == 124)); then
   printf "updating time timed out after 5 minutes"
