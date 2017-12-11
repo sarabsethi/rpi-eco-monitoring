656bf1983df44b85f5b98c59755cb7829c4a547e
davidorme <d.orme@imperial.ac.uk>
2017-12-11 11:49:42 +0000

    discover_serial.py no longer edits config.json - used to set env variable. Could revert to bash code.

--- discover_serial.py
+++ discover_serial.py
@@ -1,30 +1,26 @@
-import os
-import json
 import sys
 
-def discover_serial(config_file):
+def discover_serial():
     """
-    Function to extract Raspberry Pi serial from cpuinfo file
-    and update a config file and an environment variable
+    Function to return the Raspberry Pi serial from /proc/cpuinfo
 
     Returns:
-        A string containing the serial or an error value
+        A string containing the serial number or an error placeholder
     """
 
     # parse /proc/cpuinfo
+    cpu_serial = None
     try:
         f = open('/proc/cpuinfo', 'r')
         for line in f:
             if line[0:6] == 'Serial':
-                cpu_serial = line[10:26]
+                cpu_serial = line.split(':')[1].strip()
         f.close()
-    except:
+        # No serial line found?
+        if cpu_serial is None:
+            raise IOError
+    except IOError:
         cpu_serial = "ERROR000000001"
-
-    # store it in the config file
-    config = json.load(open(config_file, 'r'))
-    config['cpu_serial'] = cpu_serial
-    json.dump(config, open(config_file, 'w'))
 
     # echo it to std out
     sys.stdout.write(cpu_serial)
@@ -32,4 +28,4 @@ def discover_serial(config_file):
 
 if __name__ == "__main__":
 
-    discover_serial(sys.argv[1])
+    discover_serial()
